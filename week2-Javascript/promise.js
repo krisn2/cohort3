ea29e0914d5f640906043ef9e3abb8be6abcf533
krishna sarone <167612353+krisn2@users.noreply.github.com>
2025-05-22 22:09:38 +0530

    one more way to understand promise cls

--- week2-Javascript/promise.js
+++ week2-Javascript/promise.js
@@ -42,3 +42,32 @@ function fscallback(err, data) {
         console.log(data.toString());
     }
 }
+
+
+class Promise2 {
+    constructor (fn) {
+        function afterDone () {
+            this.resolve()
+        }
+        fn(afterDone)
+    }
+
+    then(callback){
+        this.resolve = callback;
+    }
+}
+
+
+function readTheFile(resolve){
+    setTimeout(function() {
+        console.log('read file');
+        resolve();
+    },2000);
+}
+
+
+function setTimeoutPromisified2() {
+    return new Promise2(readTheFile);
+}
+
+setTimeoutPromisified2().then(callback);
