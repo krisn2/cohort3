1c73f6befd87c4dc7116d7a0049bcb23ed5b2e94
krishna sarone <167612353+krisn2@users.noreply.github.com>
2025-08-27 16:03:47 +0530

    websocket in ts

---
+++ week-16-ws/src/index.ts
@@ -0,0 +1,14 @@
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer( {port:8080});
+
+wss.on("connection", function(socket){
+    console.log("User connected")
+    setInterval(() => {
+        socket.send("Current price of solana is" + Math.random());
+    },3000)
+
+    socket.on("message",(e) => {
+        console.log(e.toString())
+    })
+})
--- week5-nodejs/server.js
+++ week5-nodejs/server.js
@@ -1,24 +1,50 @@
 const express = require('express');
 const app = express();
 
-function log (req, res, next) {
-    console.log(`Method: ${req.method} URL: ${req.url}`);
-    console.log(new Date());
+const users = []
 
-    next();
+app.use(express.json());
 
-}
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-app.use(log)
-
-app.get('/sum', (req,res)=>{
-    const a =req.query.a
-    const b =req.query.b
-
-    res.json({
-        answer:parseInt(a)+parseInt(b)
-    })
+app.post('/user', (req, res) => {
+    const {name, password} = req.body;
+    let user = {name, password};
+    users.push(user);
+    res.send(user);
 })
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.get('/user/:name', (req, res) => {
+    let name = req.params.name;
+    let user = users.find(user => user.name == name);
+    res.send(user);
+})
+
+
+// function log (req, res, next) {
+//     console.log(`Method: ${req.method} URL: ${req.url}`);
+//     console.log(new Date());
+
+//     next();
+
+// }
+
+// app.use(log)
+
+// app.get('/sum', (req,res)=>{
+//     const a =req.query.a
+//     const b =req.query.b
+
+//     res.json({
+//         answer:parseInt(a)+parseInt(b)
+//     })
+// })
 
 // app.get('/multiply', (req,res)=>{
 //     const a =req.query.a
