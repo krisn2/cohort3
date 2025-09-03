d9d7f7d4fd708507dacba426167d415a24ea2742
krishna sarone <167612353+krisn2@users.noreply.github.com>
2025-09-03 17:44:19 +0530

    usecontext and createContext

--- week-9-react/hooks-class/src/App.jsx
+++ week-9-react/hooks-class/src/App.jsx
@@ -1,9 +1,21 @@
 
-import { useEffect, useState } from "react";
+import {  createContext, useEffect, useState, useContext } from "react";
+
+const BulbContext = createContext();
+
+function BulbProvider({ children }) {
+  const [bulbOn, setBulbon]= useState(true);
+
+  return (
+    <BulbContext.Provider value={{ bulbOn: bulbOn, setBulbon: setBulbon}} >
+      {children}
+    </BulbContext.Provider>
+  )
+}
+
 function App() {
 
   const [count, setcount] = useState(0);
-
   let [countervisiable, setcountervisiable] = useState(true);
 
   // useEffect( () => {
@@ -32,6 +44,15 @@ function App() {
         hi there
       </b>
      {countervisiable && <Counter count={count}/>}
+
+
+
+     {/* This is prop drilling in react passing state or data to children using middle component and it's ugly way to write code */}
+
+     {/* <Light bulbOn={bulbOn} setBulbon={setBulbon} /> */}
+     <BulbProvider>
+     <Light/>
+     </BulbProvider>
     </div>
   )
 }
@@ -70,6 +91,47 @@ function Counter(props) {
   return (
     <div>
       <h1>{props.count}</h1>
+    </div>
+  )
+}
+
+// function Light({ bulbOn,setBulbon }) {
+
+function Light() {
+  return (
+    <div>
+      {/* <LigthBulb bulbon={bulbOn}/>
+      <LightSwitch setBulbon={setBulbon}/> */}
+
+      <LigthBulb/>
+      <LightSwitch/>
+    </div>
+  )
+}
+
+// function LightSwitch({ setBulbon }){
+function LightSwitch() {
+
+  const { setBulbon } = useContext(BulbContext);
+  
+  function changeBulbstate() {
+    setBulbon( c=> !c);
+  }
+
+  return (
+    <div>
+      <button onClick={changeBulbstate}>Switch</button>
+    </div>
+  )
+}
+
+// function LigthBulb({bulbon}) {
+function LigthBulb() {
+
+  const bulbon = useContext(BulbContext).bulbOn;
+  return (
+    <div>
+      { bulbon ? <p> Bulb is on</p>: <p> Bulb is Off</p> }
     </div>
   )
 }
